14359921b475f768ffe92e310f91d3d6f797374f
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-06-05 21:43:16 -0700

    app running as designed

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,18 @@
 // Dependencies
-import { join } from "path";
+const path = require("path");
 const router = require("express").Router();
 
 router.get("/notes", (req, res) => {
-  res.sendFile(join(__dirname, "../public/notes.html"));
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 router.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // If no matching route is found directing to home page
 router.get("*", (req, res) => {
-  res.sendFile(join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-export default router;
+module.exports = router;
